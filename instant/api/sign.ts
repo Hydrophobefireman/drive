5b394b6431c28d504670243f00c76e0e0f43d318
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2023-05-08 16:58:57 -0400

    fix: make sign a serverless function instead of edge

--- instant/api/sign.ts
+++ instant/api/sign.ts
@@ -1,13 +1,10 @@
 import {GetObjectCommand, PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
 import {getSignedUrl} from "@aws-sdk/s3-request-presigner";
 import {random} from "@hydrophobefireman/kit/dist/src/util";
+import type {VercelRequest, VercelResponse} from "@vercel/node";
 
 import {buildClient} from "./_build_client";
 import {stringToHex} from "./_util";
-
-export const config = {
-  runtime: "edge",
-};
 
 const SEVEN_DAYS = 604800;
 const r2 = buildClient();
@@ -20,10 +17,13 @@ async function signUrl(config: {path: string; bucket: string}, method: string) {
   return await getSignedUrl(r2, cmd, {expiresIn: SEVEN_DAYS});
 }
 
-export default async function handle(request: Request) {
+export default async function handle(
+  request: VercelRequest,
+  response: VercelResponse
+) {
   if (request.method.toLowerCase() !== "post")
-    return new Response("err", {status: 405});
-  const {file} = await request.json();
+    return response.status(405).json({error: "invalid method"});
+  const {file} = request.body;
   const safe = stringToHex(file);
   const res = await signUrl(
     {
@@ -32,7 +32,5 @@ export default async function handle(request: Request) {
     },
     "put"
   );
-  return new Response(JSON.stringify({data: res}), {
-    headers: {"content-type": "application/json"},
-  });
+  return response.json({data: res});
 }
